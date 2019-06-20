63a634992c39b81737a842c855c630a36986e712
Morgan Skillicorn <hello@morgan.games>
2019-06-20 23:42:33 +0100

    started scripting

---
+++ Assets/BaseItem.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class BaseItem : MonoBehaviour
+{
+    public string item_name = "Insert Name";
+    public string item_description = "Insert Description";
+    public float item_weight = 0;
+    public float item_value = 0;
+    public bool item_critical = false;//critical items should not 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void AddToInventory(GameObject character)
+    {
+        if (character.GetComponent<Inventory>())
+        {
+            Inventory character_inventory = character.GetComponent<Inventory>();
+
+            if ((character_inventory.inventory_capacity_cur + item_weight) <= character_inventory.inventory_capacity_max)
+            {
+
+            }
+            else
+            {
+                Debug.Log(character.name + " does not have enough space in inventory.");
+            }
+        }
+        else
+        {
+            Debug.Log(character.name + " does not have an inventory.");
+        }
+    }
+}
---
+++ Assets/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public float inventory_capacity_max = 100;
+    public float inventory_capacity_cur = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AddItem(BaseItem item)
+    {
+
+    }
+}
